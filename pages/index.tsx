0cbb6dbb88a34e0af0ae86a7ec356f5e0acb3302
Alberto J. Perez Roman <alberto.perez8@upr.edu>
2022-01-03 23:01:31 -0400

    basic ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
-export default function Home(){
-  return(
-    <div className="text-2xl  p-2">
-      hello world!!
-    </div>
-  )
+export default function Home() {
+	return (
+		<div className="h-screen w-screen flex flex-col justify-center items-center">
+			<div className="text-2xl text-center">Pick the coolest Pokemon</div>
+      <div className="p-4"/>
+			<div className="border rounded p-8 flex justify-between max-w-2xl items-center">
+				<div className="w-16 h-16 bg-blue-500" />
+				<div className="p-8">VS</div>
+				<div className="w-16 h-16 bg-yellow-500" /> <div></div>
+			</div>
+		</div>
+	)
 }
