1a413f705773d0ab4a54403f11c4771027369127
Alberto J. Perez Roman <alberto.perez8@upr.edu>
2022-01-03 23:22:32 -0400

    restruct folders

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
 module.exports = {
-  mode: 'jit', 
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	mode: 'jit',
+	content: ['./src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
 }
